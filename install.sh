f7c30c37862502169edbc4c9b21b7de2c9d43f29
publish <publish>
2018-05-29 16:55:55 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_BUNDLE_PATH=${HOME}/.docker-bundle/bin
+
+mkdir -p ${DOCKER_BUNDLE_PATH} && cd ${DOCKER_BUNDLE_PATH}
+
+curl https://raw.githubusercontent.com/docker-bundle/docker-bundle/master/docker-bundle.py > docker-bundle
+
+chmod +x docker-bundle
+
+echo
+echo "Manually put it into your .bashrc or .zshrc:"
+
+echo '    export PATH=$HOME/.docker-bundle/bin:$PATH'
+echo
